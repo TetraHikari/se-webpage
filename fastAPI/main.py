7be424f71fca4f3e0c195c9091ac8070574a8ed1
PEERANAT PATARAKITTISOPOL <65011446@KMITL.AC.TH>
2023-10-13 13:06:22 +0700

    button link

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -65,7 +65,6 @@ def signup(username: str = Form(...), password: str = Form(...)):
     return {"success": True}
 
 
-
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
