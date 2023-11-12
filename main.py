5b13877f9cf35131252b7c749d00337a5d8c2651
Tonkla Pokaew <tonkla.p2004@gmail.com>
2023-11-13 03:16:41 +0700

    second commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Request, Form
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-
+from models import*
 import uvicorn
 
 account = {
@@ -24,12 +24,37 @@ async def login(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
 @app.post("/login")
-async def login(username: str = Form(...), password: str = Form(...)):
+async def login(request:Request,username: str = Form(...), password: str = Form(...)):
     if username in account and account[username] == password:
-        return {"status": "success"}
+        return templates.TemplateResponse("main-menu.html", {"request": request, "username": username})
     else:
         return {"status": "failure"}
+    
+@app.get("/se-blog")
+async def se_blog(request: Request):
+    # Retrieve all posts from the MongoDB collection
+    posts = collection.find({})
+    
+    # Pass the retrieved posts to the HTML template
+    return templates.TemplateResponse("blog.html", {"request": request, "posts": posts})
 
+@app.post("/delete-post/{post_id}")
+async def delete_post(request: Request, post_id: str):
+    # Delete the post from the MongoDB collection
+    collection.delete_one({"_id": ObjectId(post_id)})
+    
+    # Redirect back to the blog page
+    posts = collection.find({})
+    return templates.TemplateResponse("blog.html", {"request": request, "posts": posts})
+
+@app.post("/create-post")
+async def create_post(request: Request, title: str = Form(...), content: str = Form(...)):
+    post = Post(title=title, content=content, created_at=datetime.now())
+    collection.insert_one(post.dict())
+        # Retrieve all posts from the MongoDB collection
+    posts = collection.find({})
+    
+    return templates.TemplateResponse("blog.html", {"request": request, "posts": posts})
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
--- models.py
+++ models.py
@@ -1,5 +1,18 @@
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 from pydantic import BaseModel
+from datetime import datetime
+
+client = MongoClient('mongodb://localhost:27017')
+db = client['se-blog']
+collection = db['posts']
 
 
-
-
+class Post(BaseModel):
+    title: str
+    content: str
+    created_at: datetime = datetime.now()
+    
+    def created_at_readable(self):
+        return self.created_at.strftime("%B %d, %Y, %I:%M %p")
+    
